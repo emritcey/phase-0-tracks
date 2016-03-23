3ef9e5036dac638c9d681d5f8e5462b69584b235
emritcey <emritcey@gmail.com>
2016-03-23 11:35:23 -0500

    good candidate boolean

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -9,14 +9,25 @@ volume = gets.chomp.to_i
 puts "What color is it's fur?"
 fur_color = gets.chomp
 # Whether it is a good canditiate for adoption
+
 puts "is it a good candidate for adoption? (y/n)"
 good = gets.chomp
+
+until (good=="y" || good=="n")
+	puts "is it a good candidate for adoption? (y/n)"
+	good = gets.chomp
+end
+	if good == "y"
+		good = true
+	else 
+		good = false
+	end
+
 # estimated Age
 puts "How old do you think it is?"
-age = gets.chomp
+age = gets.chomp.to_i
 if age == ""
 	age = nil
 end
 
-puts " it is #{age}" 
 
