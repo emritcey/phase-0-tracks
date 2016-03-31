b15742bf3b477a9cb4ec4d5a40c7dd415be1019f
emritcey <emritcey@gmail.com>
2016-03-31 14:20:05 -0500

    add italian category plus items

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,25 @@
+international_foods = {
+	italian: {
+		pizza: [
+			'pepperoni', 
+			'cheese', 
+			'supreme'
+		]
+		pasta: [
+			'penne', 
+			'linguini',
+			'ravioli', 
+			'gnocci'
+		] 
+		antipasti: [
+			'caprese', 
+			'caeser salad'
+		]
+
+	}
+	mexican: {
+		tacos: []
+		burritos: []
+	}
+
+}
