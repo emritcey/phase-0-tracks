d62eb88f287c38efb54df86e748d30caf7d0f6aa
emritcey <emritcey@gmail.com>
2016-03-31 17:20:34 -0500

    edit formatting

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,6 +1,7 @@
 # Ask for real name and store.
 # Break up first and last name
 # Swap the first and last name
+
 puts "What is your first and last name?"
 full_name = gets.chomp.downcase
 name = full_name.split(' ').reverse!.join(' ')
@@ -11,8 +12,9 @@ name = name.split('')
 
 p name 
 
+#######################################################
+#other internal methods to use.
 # change all vowels to the next vowel 
-
 def next_vowel(letter)
 	if letter == ("a")
 		puts "e"
@@ -26,12 +28,16 @@ def next_vowel(letter)
 		puts "a"
 	end
 end
+
+###############
 # change all consonants to the next consonant 
 def next_consonant(letter)
 	letter.next
 end 
 
-name.map! do |letter|
+#######################################################
+
+name. do |letter|
 	if letter == ("a" || "e" || "i" || "o" || "u")
 		next_vowel(letter)
 	else 
@@ -42,3 +48,7 @@ end
 # print out new code name 
 p name
 
+
+
+
+		
