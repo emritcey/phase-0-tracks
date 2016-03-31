1409d3b8c303bce839a6cd3f09e8816cb1b6c200
emritcey <emritcey@gmail.com>
2016-03-31 18:29:15 -0500

    finished

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,52 +3,59 @@
 # Swap the first and last name
 
 puts "What is your first and last name?"
-full_name = gets.chomp.downcase
+full = gets.chomp
+full_name = full.downcase
 name = full_name.split(' ').reverse!.join(' ')
 
-p name
+#p name
 
 name = name.split('')
-
-p name 
+new_letters = []
+#p name 
 
 #######################################################
 #other internal methods to use.
 # change all vowels to the next vowel 
 def next_vowel(letter)
 	if letter == ("a")
-		puts "e"
+		letter = "e"
 	elsif letter == ("e")
-		puts "i"
+		letter = "i"
 	elsif letter == ("i")
-		puts "o"
+		letter = "o"
 	elsif letter == ("o")
-		puts "u"
+		letter = "u"
 	else
-		puts "a"
+		letter = "a"
 	end
 end
 
 ###############
 # change all consonants to the next consonant 
 def next_consonant(letter)
-	letter.next
-end 
+	c_alphabet = "bcdfghjklmnpqrstvqxyz"
+ 	x = c_alphabet.index(letter)
+  	x += 1
+	 c_alphabet[x]
+
+end
 
 #######################################################
 
-name. do |letter|
-	if letter == ("a" || "e" || "i" || "o" || "u")
-		next_vowel(letter)
-	else 
-		next_consonant(letter)
+# take each letter and assess if consonant, vowel, or space.
+
+name.each do |letter|
+	if (letter == "a") || (letter == "e") || (letter == "i") || (letter == "o") || (letter == "u")
+		new_letters << next_vowel(letter)
+	elsif letter == " "
+		new_letters << letter
+	else
+		new_letters << next_consonant(letter)
 	end
-end
+end 
 
 # print out new code name 
-p name
+new = new_letters.join.split(' ').each { |x| x.capitalize!}.join(' ')
 
+puts "#{full} is now known as #{new}."
 
-
-
-		
