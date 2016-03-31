38f02dcc057a743fed7cdd18d08edd9f2217f4cd
emritcey <emritcey@gmail.com>
2016-03-31 16:49:30 -0500

    pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,15 @@
+# Ask for real name and store.
+puts "What is your first and last name?"
+full_name = gets.chomp.downcase!
+puts full_name
+
+# Break up first and last name
+
+# Swap the first and last name
+
+# change all vowels ot the next vowel 
+
+# change all consonants to the next consonant 
+
+
+# print out new code name 
