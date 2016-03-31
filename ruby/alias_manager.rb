8a35b520ad3a910e23c78a161470dc030251f1ba
emritcey <emritcey@gmail.com>
2016-03-31 17:16:01 -0500

    add next vowel and consonant methods, and begin map code

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,15 +2,43 @@
 # Break up first and last name
 # Swap the first and last name
 puts "What is your first and last name?"
-full_name = gets.chomp.downcase!
-name = full_name.split(' ').reverse!
+full_name = gets.chomp.downcase
+name = full_name.split(' ').reverse!.join(' ')
 
-puts name
+p name
 
+name = name.split('')
+
+p name 
 
 # change all vowels to the next vowel 
 
+def next_vowel(letter)
+	if letter == ("a")
+		puts "e"
+	elsif letter == ("e")
+		puts "i"
+	elsif letter == ("i")
+		puts "o"
+	elsif letter == ("o")
+		puts "u"
+	else
+		puts "a"
+	end
+end
 # change all consonants to the next consonant 
+def next_consonant(letter)
+	letter.next
+end 
 
+name.map! do |letter|
+	if letter == ("a" || "e" || "i" || "o" || "u")
+		next_vowel(letter)
+	else 
+		next_consonant(letter)
+	end
+end
 
 # print out new code name 
+p name
+
