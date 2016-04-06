9e12134a15d557654f09339f9d334af0d55cd44b
emritcey <emritcey@gmail.com>
2016-04-06 11:42:30 -0500

    add more characteristics

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,15 +1,18 @@
 # SPECIES ------------------------
-# Puppy
+# Puppy Dog
 
 # CHARACTERISTICS ----------------
-# Fur: Soft
+# Soft Fur: yes
 # Friendly: yes
 # Leg Count: 4
 # Color: varies
 # Name: varies
+# Energy Level: high
 
 # BEHAVIOR -----------------------
 # Jump
 # Play
 # Fetch
 # Bark
+# Run
+# Nap
