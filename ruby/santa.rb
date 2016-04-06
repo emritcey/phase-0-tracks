48565506fee2c0ff53bb939c91ca387144f51c59
emritcey <emritcey@gmail.com>
2016-04-06 16:49:42 -0500

    add attributes, add each code, add getter, and change methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,9 @@
 class Santa 
-	def initialize
+	def initialize(gender, ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity 
+		@age = 0
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		puts "Initializing Santa instance ..."
 	end
 
@@ -8,10 +12,40 @@ class Santa
 	end
 
 	def eat_milk_and_cookies(cookie)
-		puts "THat was a good #{cookie} cookie!"
+		puts "That was a good #{cookie} cookie!"
+	end
+	
+	def celebrate_birthday
+		new_age = @age + 1 
+		puts new_age
+	end 
+	
+	def get_mad_at(reindeer)
+		@reindeer_ranking.delete(reindeer)
+		@reindeer_ranking.push(reindeer)
+		puts "New order: #{@reindeer_ranking}"
+	end
+	
+	def gender
+		p @gender
+	end
+	
+	def ethnicity
+		p @ethnicity
 	end
 end 
 
-emma = Santa.new
-emma.speak
-emma.eat_milk_and_cookies("Chocolate")
+santas = []
+santas << Santa.new("female", "white")
+santas << Santa.new("male", "black")
+santas << Santa.new("female", "Japanese")
+
+santas.each do |name|
+	name.speak
+	name.eat_milk_and_cookies("Chocolate")
+	name.celebrate_birthday
+	name.get_mad_at("Rudolph")
+	name.gender
+	name.ethnicity
+	puts " "
+end
