fb4d108dcb331dfc3de7addd9e937c7c7a7a04c4
emritcey <emritcey@gmail.com>
2016-04-06 11:36:34 -0500

    add class framework

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Fur: Soft
+# Friendly: yes
+# Leg Count: 4
+# Color: varies
+# Name: varies
+
+# BEHAVIOR -----------------------
+# Jump
+# Play
+# Fetch
+# Bark
