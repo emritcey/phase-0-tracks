7bc8aa3c41d44fdd7b0821fcd6bae528f2772c84
emritcey <emritcey@gmail.com>
2016-04-06 13:30:05 -0500

    add trick methods, and checkout answers at bottom

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,35 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
-
+  
+  def speak(number)
+    number.times {puts "Woof!"}
+  end
+  
+  def roll_over
+    puts "*Rolls over*"
+  end 
+  
+  def dog_years(human_yr)
+    new_year = human_yr * 7
+    puts "#{human_yr} humans years is #{new_year} in dog years!"
+  end 
+  
+  def shake(paw)
+    if paw == "right"
+      puts "*Shakes right paw*"
+    elsif paw == "left"
+      puts "*Shakes left paw*"
+    else 
+      puts "That wasn't a paw!"
+    end
+  end 
 end
 
+rufus = Puppy.new
+rufus.fetch("ball") 
+rufus.speak(5)
+rufus.roll_over
+rufus.dog_years(7)
+rufus.shake("left")
+rufus.shake(7)
