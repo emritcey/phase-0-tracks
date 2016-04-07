c01d4fdf57774195a7671df0d8b67fea21db9af7
emritcey <emritcey@gmail.com>
2016-04-06 20:24:47 -0500

    add formatting for readable code, add arrays for age and categories, add times to do it, add sample

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,42 +5,55 @@ class Santa
 	def initialize(gender, ethnicity)
 		@gender = gender
 		@ethnicity = ethnicity 
-		@age = 0
+		@age = (0..140).to_a
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		puts "Initializing Santa instance ..."
 	end
 
 	def speak
-		puts "Ho, ho, ho! Haaaappy holidays!"
+		"Ho, ho, ho! Haaaappy holidays!"
 	end
 
 	def eat_milk_and_cookies(cookie)
-		puts "That was a good #{cookie} cookie!"
+		"That was a good #{cookie} cookie!"
 	end
 	
 	def celebrate_birthday
-		new_age = @age + 1 
-		puts new_age
+	
+		new_age = @age.sample + 1 
 	end 
 	
 	def get_mad_at(reindeer)
 		@reindeer_ranking.delete(reindeer)
 		@reindeer_ranking.push(reindeer)
-		puts "New order: #{@reindeer_ranking}"
+		puts "Mad at: #{reindeer}!"
 	end
 end 
 
+
+# -----------------------------------
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+genders = (0..example_genders.length).to_a
+
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A", "Filipino", "Norweigen"]
+ethnicities = (0..example_ethnicities.length).to_a
+
+example_cookies = ["snickerdoodle", "chocolate chip", "sugar", "lemon", "frosted"]
+cookies = (0..example_cookies.length).to_a
+
+reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+rr = (0..reindeer_ranking.length).to_a
+# -----------------------------------
+
 santas = []
-santas << Santa.new("female", "white")
-santas << Santa.new("male", "black")
-santas << Santa.new("female", "Japanese")
+2.times do
+  santas << Santa.new(example_genders[genders.sample], example_ethnicities[ethnicities.sample])
+end
+
 
 santas.each do |name|
-	name.speak
-	name.eat_milk_and_cookies("Chocolate")
-	name.celebrate_birthday
-	name.get_mad_at("Rudolph")
-	puts  name.gender
-	puts name.ethnicity
-	puts " "
+	puts "Santa says: #{name.speak}. #{name.eat_milk_and_cookies(example_cookies[cookies.sample])} "
+	puts  "Age: #{name.celebrate_birthday}. Gender: #{name.gender}. Ethnicity: #{name.ethnicity}."
+		name.get_mad_at(reindeer_ranking[rr.sample])
+	puts "----------------------------------- "
 end
