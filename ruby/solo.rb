3e32ccd9a338a0f5a373767cbb187921b3806e87
emritcey <emritcey@gmail.com>
2016-04-06 21:03:12 -0500

    begin user interface: puts output, loops for number of times user wants to use

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -14,10 +14,46 @@ class Car
 	end
 
 	def miles(number)
-		puts "Drove #{number} miles today."
+		@milage += number
+		puts "Drove #{number} miles today. New milage: #{@milage}"
+		
 	end
 
-	def destination
-		puts "I am driving to #{destination}."
+	def destination(destin)
+		puts "I am driving to #{destin}."
 	end
 end
+
+# steve = Car.new("audi", 2008)
+# puts steve.sound
+# puts steve.miles(500)
+# puts steve.destination("LA")
+# puts "-------------------"
+# steve = Car.new("VW", 2016)
+# puts steve.sound
+# puts steve.miles(0)
+# puts steve.destination("Nowhere.")
+
+# ===========================================
+puts "How many cars would you like to create?"
+number = gets.chomp.to_i
+
+until number == 0 
+
+	puts "#{number} cars left: What make?"
+	make = gets.chomp.downcase
+
+	puts "What year?"
+	year = gets.chomp.to_i
+
+	puts "What is it's milage?"
+	milage = gets.chomp.to_i
+
+	puts "What is your destination?"
+	destin = gets.chomp
+
+	number -= 1
+end
+
+puts "#{make} #{year} #{milage}"
+
