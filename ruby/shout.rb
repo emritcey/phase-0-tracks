9353c349553f91a866c4c11d20a5da091a59dcaa
emritcey <emritcey@gmail.com>
2016-04-09 13:43:17 -0500

    add new mixin shout, add new 2 classes and driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,34 @@
-module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
-	end
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
 
-	def self.yelling_happily(words)
-		words + "!!!" + " :)"
-	end
+# 	def self.yelling_happily(words)
+# 		words + "!!!" + " :)"
+# 	end
 	
-	p self.yell_angrily("I hate you")
-	p self.yelling_happily("Oh my gosh")
+# 	p self.yell_angrily("I hate you")
+# 	p self.yelling_happily("Oh my gosh")
+# end
+
+# ===========================================
+
+module Shout 
+	def yell(who, words)
+		puts "#{who.capitalize} is yelling #{words}!!!"
+	end
 end
 
+class Crowd
+	include Shout
+end
 
+class Kid
+	include Shout
+end
+
+crowd = Crowd.new
+crowd.yell("crowd", "ONE MORE SONG")
+
+kid = Kid.new
+kid.yell("kid", "HELLO")
