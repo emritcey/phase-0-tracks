6d1d0914bf714c852bdcc041918c499fa4d8ad58
emritcey <emritcey@gmail.com>
2016-04-13 14:44:57 -0500

    add for loop to iterate through two arrays and putinto color_Horses

--- js/data_structures.js
+++ js/data_structures.js
@@ -6,3 +6,18 @@ horse_Names.push("Ed");
 
 console.log(colors)
 console.log(horse_Names)
+
+// create a new variable for the array
+// loop through number of items in each group 
+// pair them together. 
+
+// for each index in the names array, take the name and 
+//make it the key, and the matching color and make it the value
+
+var color_Horses = [];
+
+for (var i = 0; i < colors.length; i++) {
+color_Horses[horse_Names[i]] = colors[i];
+}
+
+console.log(color_Horses)
