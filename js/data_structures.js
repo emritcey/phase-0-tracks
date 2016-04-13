15ba8ff09a0de6b27f9c4ba7239e9216773da2e0
emritcey <emritcey@gmail.com>
2016-04-13 14:27:56 -0500

    add push fuctions

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,2 +1,8 @@
 var colors = ["blue", "pink", "turquoise", "red"];
 var horse_Names = ["Charles", "Petunia", "Scout", "Mojo"];
+
+colors.push("purple");
+horse_Names.push("Ed");
+
+console.log(colors)
+console.log(horse_Names)
