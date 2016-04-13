be04ec0f3ec0160584c4cdf686705099a76f0167
emritcey <emritcey@gmail.com>
2016-04-13 16:57:08 -0500

    script for 2 times around

---
+++ js/algos.js
@@ -0,0 +1,49 @@
+var list = ["fishes", "sam", "pizza", "washington"];
+// console.log(list.length);
+
+// count letters in each word and store into an array
+var numbers =[];
+for (var i = 0; i < list.length; i++) {
+	numbers.push(list[i].length);
+}	
+console.log(numbers);
+
+// if 1st is bigger than 2nd then delete 2nd. 
+var second_Arr = [];
+var third_Arr = [];
+
+for (var i = 0; i < numbers.length; i++) {
+	var x = numbers[i];
+	var y = numbers[i+=1];
+	console.log(x,y);
+
+if (x > y) {
+	second_Arr.push(x);
+} else if (x < y) {
+	second_Arr.push(y);
+} else {
+	second_Arr.push(x, y);
+}
+}
+
+for (var i = 0; i < second_Arr.length; i++) {
+	var x = second_Arr[i];
+	var y = second_Arr[i+=1];
+console.log(x,y);
+
+if (x > y) {
+	third_Arr.push(x);
+} else if (x <= y) {
+	third_Arr.push(y);
+} else {
+	third_Arr.push(x, y);
+}
+}
+
+console.log(third_Arr)
+var chosen = third_Arr[0]
+var longest = (list[numbers.indexOf(chosen)])
+
+// print 
+console.log("The longest word is " + longest + ".");
+
