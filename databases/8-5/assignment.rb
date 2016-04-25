48b2bd01ccffa72396c131b9610ac5adcf15dec9
emritcey <emritcey@gmail.com>
2016-04-24 20:03:39 -0500

    add user interface to ask waht tiems to add

--- databases/8-5/assignment.rb
+++ databases/8-5/assignment.rb
@@ -26,30 +26,54 @@ def create_item(db, clothing_type, color, year_bought)
 	db.execute("INSERT INTO closet (clothing_type, color, year_bought) VALUES (?, ?, ?)", [clothing_type, color, year_bought])
 end
 ##################################################################
+#USER INTERFACE SECTION
 # Ask user, on a loop, what item they would like to create and how many until they say done. 
-
-5.times do 
-	create_item(db, "shirt", Faker::Commerce.color, 2016) 
-end
-
-
+puts "----------------------------------------------------"
+puts "Hi! Welcome to Closet Master! We are here to build a closet for you so you don't have to!!"
+puts "We will prompt you, and then just let us know how many of each item you want!"
+puts "----------------------------------------------------"
+puts "Ok! Let's get started! How many shirts would you like?"
+	x = gets.chomp.to_i
+	x.times do 
+		create_item(db, "shirt", Faker::Commerce.color, 2016) 
+	end
+########################
+puts "---------------------"
+puts "Great! We will pick #{x} of shirts out!"
+puts "How many pairs of pants would you like?"
+	y = gets.chomp.to_i
+	y.times do 
+		create_item(db, "pants", Faker::Commerce.color, 2016) 
+	end
+########################
+puts "---------------------"
+puts "Great! We will pick #{y} of pants out!"
+puts "How many skirts would you like?"
+	z = gets.chomp.to_i
+	z.times do 
+		create_item(db, "skirts", Faker::Commerce.color, 2016) 
+	end
+########################
+puts "---------------------"
+puts "Great! We will pick #{z} of skirts out!"
+puts "How many pairs of shoes would you like?"
+	k = gets.chomp.to_i
+	k.times do 
+		create_item(db, "shoes", Faker::Commerce.color, 2016) 
+	end
+########################
 
 
 #tell user what they have in thier closet and 
-
 puts "-----------------"
 puts "Closet Inventory:"
 
 closet = db.execute("SELECT * FROM closet")
 closet.each do |closet|
-	puts "1 #{closet['color']} #{closet['clothing_type']} from #{closet['year_bought']}."
+	puts "1 -- #{closet['color']} #{closet['clothing_type']} from #{closet['year_bought']}."
 end 
-
-#prints out lists of all the clothes 
-# asks if you want to get rid of any items, maybe 
-#goodwill or throw away.
-
 puts "-----------------"
+
 
 # puts "Would you like to delete anything from your closet?"
 
