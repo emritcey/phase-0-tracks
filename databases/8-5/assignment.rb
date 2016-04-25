5ea687f07293f68cdb2d2259f9318ab85f6ddfaf
emritcey <emritcey@gmail.com>
2016-04-24 21:22:28 -0500

    add delete option and looping for invalid entries

--- databases/8-5/assignment.rb
+++ databases/8-5/assignment.rb
@@ -35,34 +35,62 @@ puts "----------------------------------------------------"
 puts "Ok! Let's get started! How many shirts would you like?"
 	x = gets.chomp.to_i
 	x.times do 
-		create_item(db, "shirt", Faker::Commerce.color, 2016) 
+		create_item(db, "Shirt", Faker::Commerce.color, 2016) 
 	end
 ########################
 puts "---------------------"
-puts "Great! We will pick #{x} of shirts out!"
+puts "Great! We will pick #{x} shirts out!"
+puts " "
 puts "How many pairs of pants would you like?"
 	y = gets.chomp.to_i
 	y.times do 
-		create_item(db, "pants", Faker::Commerce.color, 2016) 
+		create_item(db, "Pants", Faker::Commerce.color, 2016) 
 	end
 ########################
 puts "---------------------"
-puts "Great! We will pick #{y} of pants out!"
+puts "Great! We will pick #{y} pants out!"
+puts " "
 puts "How many skirts would you like?"
 	z = gets.chomp.to_i
 	z.times do 
-		create_item(db, "skirts", Faker::Commerce.color, 2016) 
+		create_item(db, "Skirts", Faker::Commerce.color, 2016) 
 	end
 ########################
 puts "---------------------"
-puts "Great! We will pick #{z} of skirts out!"
+puts "Great! We will pick #{z} skirts out!"
+puts " "
 puts "How many pairs of shoes would you like?"
 	k = gets.chomp.to_i
 	k.times do 
-		create_item(db, "shoes", Faker::Commerce.color, 2016) 
+		create_item(db, "Shoes", Faker::Commerce.color, 2016) 
 	end
 ########################
+puts "---------------------"
+puts " "
+wants_to_add = " "
 
+until (wants_to_add == "yes") || (wants_to_add == "no")
+	puts "Are there any other items we didn't mention that you would like to add?"
+	wants_to_add = gets.chomp.downcase
+
+		if (wants_to_add == "yes") 
+			puts "What item would you like to add?"
+			add_item = gets.chomp.capitalize
+			puts "How many of these would you like?"
+			quantity = gets.chomp.to_i
+			puts "Great! We will make sure to grab #{quantity} #{add_item}s!"
+
+				quantity.times do 
+					create_item(db, "#{add_item}", Faker::Commerce.color, 2016) 
+				end
+
+		elsif (wants_to_add == "no")
+			puts "Ok we won't add anything else!"
+
+		else
+			puts "That was an invalid entry. Please type 'yes' or 'no'."
+		end
+end
 
 #tell user what they have in thier closet and 
 puts "-----------------"
@@ -70,11 +98,36 @@ puts "Closet Inventory:"
 
 closet = db.execute("SELECT * FROM closet")
 closet.each do |closet|
-	puts "1 -- #{closet['color']} #{closet['clothing_type']} from #{closet['year_bought']}."
+	puts "#{closet['id']}: 1 -- #{closet['color']} #{closet['clothing_type']}. (purchased: #{closet['year_bought']})"
 end 
 puts "-----------------"
 
+#################################################
+#ask if you want to delete anything
+delete = ""
 
-# puts "Would you like to delete anything from your closet?"
+until (delete == "yes") || (delete == "no")
+	puts "Would you like to delete anything from your closet? (yes/no)"
+	delete = gets.chomp.downcase
 
-# FAKER::Commerce.color
+		if delete == "yes"
+			puts "Which item in the list would you like to delete?"
+			rid = gets.chomp
+			db.execute("DELETE FROM closet WHERE id = '#{rid}'")
+			puts "Ok great, we have deleted number #{rid}!"
+				puts "-----------------"
+				puts "NEW Closet Inventory:"
+				closet = db.execute("SELECT * FROM closet")
+				closet.each do |closet|
+					puts "#{closet['id']}: 1 -- #{closet['color']} #{closet['clothing_type']}. (purchased: #{closet['year_bought']})"
+				end 
+
+		elsif delete == "no"
+			puts "Ok! We will leave the list as is!"
+		else 
+			puts "INVALID ENTRY"
+		end
+end 
+
+puts "-----------------"
+puts "Thank you for playing! We hope you like your new wardrobe!!"
