1aeeb8979cb2b1a2d408ccfa6473f9e613881699
emritcey <emritcey@gmail.com>
2016-04-24 19:09:45 -0500

    add create table instructions

--- databases/8-5/assignment.rb
+++ databases/8-5/assignment.rb
@@ -1,5 +1,17 @@
+#require gems 
+
 require 'sqlite3'
 require 'faker'
 
 db = SQLite3::Database.new("assignment.db")
-
+# create table for the closet to hold items 
+create_table_cmd = <<-SQL 
+	CREATE TABLE IF NOT EXISTS closet (
+	id INTEGER PRIMARY KEY,
+	clothing_type VARCHAR(255),
+	color VARCHAR(255),
+	year_bought INT,
+	)
+SQL
+ 
+db.execute(create_table_cmd)
