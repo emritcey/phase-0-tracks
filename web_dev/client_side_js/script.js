dca0b8e62454c871128b3d688c33c054cffcf42d
emritcey <emritcey@gmail.com>
2016-04-27 16:56:56 -0500

    add 2 functions for header and text color

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,5 +1,22 @@
 console.log("The script is running! ");
+// Practice from Video:
+// var photo = document.getElementById("puppy");
+// photo.style.border = "3px solid green";
 
-var photo = document.getElementById("puppy");
+function addFontColor (event) {
+	var photo = document.getElementById("tex");
+	photo.style.color = "pink";
+	photo.style.fontSize = "50px"
+}
 
-photo.style.border = "3px solid green";
+var photo = document.getElementById("tex");
+photo.addEventListener("click", addFontColor);
+
+
+function addHeaderColor (event) {
+	event.target.style.color = "pink";
+	// photo.style.fontSize = "50px"
+}
+
+var photo = document.getElementById("main");
+photo.addEventListener("click", addHeaderColor);
